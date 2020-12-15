441324e46a737df733f53f515b792e258c67993f
Your Name <you@example.com>
2020-12-15 13:30:11 -0600

    first commit

--- mail-service/server.js
+++ mail-service/server.js
@@ -4,14 +4,20 @@ require('dotenv').config()
 const morgan = require('morgan')
 const nodemailer = require('nodemailer')
 const port = 3002
+const path = require('path')
 
 app.use(morgan('dev'))
 app.use(express.json())
 
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+
 app.use('/sendtome', require('./routes/sendToMe'))
 
 
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 
 app.listen(port, () => {
